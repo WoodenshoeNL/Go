a0d707bb0e49eaad4e9f20921eec008785ed09a6
Michel Klomp <michel.klomp@sentia.com>
2019-09-08 22:44:08 +0200

    update add location

--- go-api-time/build-apiDockerImage.sh
+++ go-api-time/build-apiDockerImage.sh
@@ -8,12 +8,10 @@ ACR_NAME="woodregistry"
 #Create API Dockerfile
 
 az acr build \
-    --registry $ACR_NAME \
+    --registry $ACR_NAME https://github.com/WoodenshoeNL/Go.git \
     --resource-group $ResourceGroup \
-    --file go-api-time/Dockerfile \
-    --image woodGo/time-api https://github.com/WoodenshoeNL/Go.git
-
-
+    --file go-api-time/dockerfile \
+    --image woodgo/gotimeapi
 
 
 #show Images
@@ -22,3 +20,10 @@ az acr repository list \
     --resource-group $ResourceGroup \
     --name $ACR_NAME \
     --output table
+
+
+
+#az acr build \
+#    --registry $ACR_NAME . \
+#    --resource-group $ResourceGroup \
+#    --image smilr/gotimeapi
