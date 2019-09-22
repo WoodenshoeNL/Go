b065d136a5ab3effa4fe5820bf21fff79781de45
Michel Klomp <michel.klomp@sentia.com>
2019-09-22 13:34:42 +0200

    change hello world text

--- plural-go-web-intro/src/main.go
+++ plural-go-web-intro/src/main.go
@@ -6,7 +6,7 @@ import (
 
 func main() {
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		w.Write([]byte("Hello, World"))
+		w.Write([]byte("Hello, World 2.0"))
 	})
 	http.ListenAndServe(":8000", nil)
 }
