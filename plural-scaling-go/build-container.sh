287c533ff727a84e6c24ea16f6e5fce8bd0387f1
Michel Klomp <michel.klomp@sentia.com>
2019-09-22 23:14:55 +0200

    update docker files go scaling

--- plural-scaling-go/build-container.sh
+++ plural-scaling-go/build-container.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 
-docker build -t woodenshoe/plural-web .
+docker build -f ./Dockerfile-web -t woodenshoe/plural-scale-web .
+
+docker build -f ./Dockerfile-dataservice -t woodenshoe/plural-scale-data .
 
 #docker run --rm woodenshoe/plural-web
 #docker run -p 8000:8000 -it woodenshoe/plural-web sh
