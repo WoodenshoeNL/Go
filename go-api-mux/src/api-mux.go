d50590e65d000e11c684eeeb6c93bc0a72e87013
Michel Klomp <michel.klomp@sentia.com>
2019-09-29 22:18:11 +0200

    change go mux to queries

--- go-api-mux/src/api-mux.go
+++ go-api-mux/src/api-mux.go
@@ -6,23 +6,22 @@ import (
     "time"
     "github.com/gorilla/mux"
 )
-// ArticleHandler is a function handler
-func ArticleHandler(w http.ResponseWriter, r *http.Request) {
-    // mux.Vars returns all path parameters as a map
-    vars := mux.Vars(r)
+func QueryHandler(w http.ResponseWriter, r *http.Request) {
+    // Fetch query parameters as a map
+    queryParams := r.URL.Query()
     w.WriteHeader(http.StatusOK)
-    fmt.Fprintf(w, "Category is: %v\n", vars["category"])
-    fmt.Fprintf(w, "ID is: %v\n", vars["id"])
+    fmt.Fprintf(w, "Got parameter id:%s!\n", queryParams["id"][0])
+    fmt.Fprintf(w, "Got parameter category:%s!", queryParams["category"][0])
 }
 func main() {
     // Create a new router
     r := mux.NewRouter()
     // Attach a path with handler
-    r.HandleFunc("/articles/{category}/{id:[0-9]+}", ArticleHandler)
+    r.HandleFunc("/articles", QueryHandler)
+    r.Queries("id", "category")
     srv := &http.Server{
         Handler: r,
         Addr: ":8000",
-        // Good practice: enforce timeouts for servers you create!
         WriteTimeout: 15 * time.Second,
         ReadTimeout: 15 * time.Second,
     }
